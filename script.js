6741283a9a409801a5cfb76b10b2031c6fb94d6a
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-04-15 23:57:22 +0000

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log('running on',location.host);
+const a = document.createElement('a')
+const body = document.querySelector('body');
+body.append(a)
+a.innerHTML = "The Website Has been started on "+
+ location.host;
