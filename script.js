2ab5e22c1b90b9e765442f80a5331bf3ee32c862
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-04-16 00:01:36 +0000

    update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ const a = document.createElement('a')
 const body = document.querySelector('body');
 body.append(a)
 a.innerHTML = "The Website Has been started on "+
- location.host;
+ location.port;
