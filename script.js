109fc757c90348330269ca5dcc5190f32e239ebd
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-04-18 21:49:05 +0000

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function discordRedirect(){
-	
-	}
+console.log(
+ "The Website Has been started on",
+ location.host);
+const body = document.querySelector(
+ 'body');
+const p = document.querySelectorAll(
+ "p")
+let isBlackmode = true;
+
+function changeMode() {
+ if (isBlackmode) {
+  body.style.background = "white";
+  body.style.color = "black";
+ } else {
+  body.style.background = "black";
+  body.style.color = "white";
+ }
+ isBlackmode = !isBlackmode;
+}
