f0ed82c0bfdf04cd3135ed28e4c28190aef71200
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-04-23 22:17:31 +0000

    update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-console.log(
- "The Website Has been started on",
- location.host);
-const body = document.querySelector(
- 'body');
-const p = document.querySelectorAll(
- "p")
-let isBlackmode = true;
-
-function changeMode() {
-  body.style.background = isBlackmode ? "white" : "black";
-  body.style.color = isBlackmode ? "black" : "white";
- isBlackmode = !isBlackmode;
+const body = document.querySelector('body');
+ const h1 = document.querySelector('h1');
+const p = document.querySelectorAll(
+ "p");
+const a = document.querySelectorAll("a");
+let isBlackmode = true;
+
+function changeMode() {
+  let colors = [isBlackmode ? "black" : "white",isBlackmode ? "black" : "cyan"]
+  body.style.background = isBlackmode ? "linear-gradient(-135deg,white,gray,gray)" : "linear-gradient(-135deg,gray,black,black)";
+  body.style.color = colors[0];
+  h1.style.color = colors[1];
+  a.forEach(x => x.style.color = colors[1]);
+  p.forEach(x => x.style.borderColor = colors[0]);
+ isBlackmode = !isBlackmode;
 }
