db540de1424c5947646e6075fea3e6cb8feb530e
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-05-07 00:57:35 +0000

    update script.js

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
+const theme = localStorage.getItem("theme");
+if (!theme) {
+  document.documentElement.setAttribute("theme", "dark");
+  localStorage.setItem("dark")
+} else document.documentElement.setAttribute("theme", theme);
 const body = document.querySelector('body');
 
- const h1 = document.querySelector('h1');
- 
- const h3 = document.querySelector('h3');
+const h1 = document.querySelector('h1');
+const h3 = document.querySelector('h3');
 
 const p = document.querySelectorAll("p");
 
 const a = document.querySelectorAll("a");
 
-let isBlackmode = true;
-
 
 
 function changeMode() {
-
-  let colors = [isBlackmode ? "black" : "white",isBlackmode ? "black" : "cyan"]
-
-  body.style.background = isBlackmode ? "linear-gradient(-135deg,white,gray,gray)" : "linear-gradient(-135deg,gray,black,black)";
-
-  body.style.color = colors[0];
-  h1.style.color = h3.style.color = colors[1];
-
-  a.forEach(x => x.style.color = colors[1]);
-
-  p.forEach(x => x.style.borderColor = colors[0]);
-
- isBlackmode = !isBlackmode;
-
+  let wanted = localStorage.getItem("theme") == "dark" ? "light" : "dark"
+  document.documentElement.setAttribute("theme", wanted)
+  localStorage.setItem("theme", wanted);
 }
-
