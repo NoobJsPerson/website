0f4c2d8e7a5071a69a2df213c8290a5de5e115de
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-05-12 14:59:12 +0000

    update script.js

--- script.js
+++ script.js
@@ -4,16 +4,10 @@ if (!theme) {
   localStorage.setItem("dark")
 } else document.documentElement.setAttribute("theme", theme);
 const body = document.querySelector('body');
-
 const h1 = document.querySelector('h1');
 const h3 = document.querySelector('h3');
-
 const p = document.querySelectorAll("p");
-
 const a = document.querySelectorAll("a");
-
-
-
 function changeMode() {
   let wanted = localStorage.getItem("theme") == "dark" ? "light" : "dark"
   document.documentElement.setAttribute("theme", wanted)
