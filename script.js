8e6ad178d3be4d7b9812b9e9d6ded7494bd569bb
itsmeac101@gmail.com <itsmeac101@gmail.com>
2021-07-08 12:54:59 +0100

    fixes setItem error

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-const themes = localStorage.getItem("theme");
-if (!themes) {
+const theme = localStorage.getItem("theme");
+if (!theme) {
   document.documentElement.setAttribute("theme", "dark");
   localStorage.setItem("theme","dark")
 } else document.documentElement.setAttribute("theme", theme);
-const body = document.querySelector('body');
-const h1 = document.querySelector('h1');
-const h3 = document.querySelector('h3');
-const p = document.querySelectorAll("p");
-const a = document.querySelectorAll("a");
+
 function changeMode() {
   let wanted = localStorage.getItem("theme") == "dark" ? "light" : "dark"
   document.documentElement.setAttribute("theme", wanted)
